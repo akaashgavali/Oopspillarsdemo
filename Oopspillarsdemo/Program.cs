a2079141b153979e3da652fe03266127c1cc4f15
akaashgavali <akashgavali4546@gmail.com>
2022-10-13 11:13:45 +0530

    Add project files.

---
+++ Oopspillarsdemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopspillarsdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methodoverloading c1 = new Methodoverloading();
+            Console.WriteLine(c1.Add(1, 2, 3));
+            Console.WriteLine(c1.Add(2, 2));
+            Console.WriteLine(c1.Add(3.4, 4));
+            Console.WriteLine(c1.Add(5, 4.5));
+
+        }
+    }
+}
